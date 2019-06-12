3ff45fbf0cad69f9dc1fc4f37e580ddf7112c74d
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2019-06-13 01:56:10 +0200

    use note detection and mode + third heuristic

--- oiga/analysis/key.py
+++ oiga/analysis/key.py
@@ -5,28 +5,38 @@ import matplotlib.pyplot as plt
 class Key:
     def __init__(self, path):
         self.src = aubio.source(path, channels=1)
-        self.find_pitch = aubio.pitch('fcomb', samplerate=self.src.samplerate)
         self.find_note = aubio.notes('default', samplerate=self.src.samplerate)
     
     def find_notes(self):
         return np.array([
-            self.find_pitch(block)[0]
-            for block in self.src
-            if len(block) == self.find_pitch.hop_size and self.find_pitch(block)
+            int(note) % 12 for note in (
+                self.find_note(block)[0]
+                for block in self.src
+                if len(block) == self.find_note.hop_size
+            ) if note != 0
         ])
-    
-    def frequency_to_pitch(self, frequency):
-        return ((np.rint(12 * np.log2(frequency / 440)) - 3) % 12).astype(int)
 
     def key_signature(self):
-        notes = self.frequency_to_pitch(self.find_notes())
+        notes = self.find_notes()
         distribution = np.bincount(notes)
 
-        pitches = np.arange(12)
         labels = ['C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab', 'A', 'Bb', 'B']
+        tonic = np.argmax(distribution)
+        tonic_note = labels[tonic]
+        minor_third = (tonic + 3) % 12
+        major_third = (tonic + 4) % 12
+        
+        if distribution[major_third] > distribution[minor_third]:
+            mode = 'major'
+        else:
+            mode = 'minor'
+        
+        pitches = np.arange(12)
         plt.bar(pitches, distribution)
         plt.xticks(pitches, labels)
         plt.show()
+        
+        return (tonic_note, mode)
 
 if __name__ == '__main__':
     import sys
