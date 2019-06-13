50e48823635f2536bafb37facbe80a0d138f300b
Imegirin <imegirin@gmail.com>
2019-06-13 10:56:56 +0200

    further develop of predicting pipeline

--- oiga/genre.py
+++ oiga/genre.py
@@ -19,8 +19,12 @@ class GenreClassifier:
     def predict(self, path):
         sample_rate, track = read_mp3(path)
         track = np.average(track, axis=1)
+        expected_genre = []
         for spec in generate_spectrograms(track, sample_rate, 12):
-            print(self.model.predict(spec))
+            spec = spec.reshape(1, 1, spec.shape[0], spec.shape[1]).astype('float32')
+            expected_genre.append(np.argmax(self.model.predict(spec)[0]))
+        counts = np.bincount(expected_genre)
+        return np.argmax(counts)
 
 if __name__ == '__main__':
     import sys
