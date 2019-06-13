a777bb4f0abdb42d9e498019df77c505bfbece1d
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2019-06-13 10:45:06 +0200

    output analysis results

--- oiga/analysis/key.py
+++ oiga/analysis/key.py
@@ -3,6 +3,15 @@ import aubio
 import matplotlib.pyplot as plt
 
 class Key:
+    NOTES = ['C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab', 'A', 'Bb', 'B']
+    NOTES_SHARP = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+    NOTES_FLAT = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B']
+
+    KEYS_WITH_SHARPS = {
+        'major': ['G', 'D', 'A', 'E', 'B'],
+        'minor': ['E', 'B', 'F#', 'C#', 'G#']
+    }
+
     def __init__(self, path):
         self.src = aubio.source(path, channels=1)
         self.find_note = aubio.notes('default', samplerate=self.src.samplerate)
@@ -18,25 +27,30 @@ class Key:
 
     def key_signature(self):
         notes = self.find_notes()
-        distribution = np.bincount(notes)
+        self.distribution = np.bincount(notes)
 
-        labels = ['C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab', 'A', 'Bb', 'B']
-        tonic = np.argmax(distribution)
-        tonic_note = labels[tonic]
+        tonic = np.argmax(self.distribution)
+        tonic_note = self.NOTES[tonic]
         minor_third = (tonic + 3) % 12
         major_third = (tonic + 4) % 12
         
-        if distribution[major_third] > distribution[minor_third]:
+        if self.distribution[major_third] >= self.distribution[minor_third]:
             mode = 'major'
         else:
             mode = 'minor'
         
-        pitches = np.arange(12)
-        plt.bar(pitches, distribution)
-        plt.xticks(pitches, labels)
-        plt.show()
-        
-        return (tonic_note, mode)
+        self.detected = (tonic_note, mode)
+        return self.detected
+    
+    def note_distribution(self):
+        hist = self.distribution / self.distribution.max()
+
+        if self.detected[0] in self.KEYS_WITH_SHARPS[self.detected[1]]:
+            notes = self.NOTES_SHARP
+        else:
+            notes = self.NOTES_FLAT
+
+        return dict(zip(notes, hist))
 
 if __name__ == '__main__':
     import sys
